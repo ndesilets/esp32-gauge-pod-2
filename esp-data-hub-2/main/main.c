746e036a4dc320f1286eb2b4c1faaf173cddb287
Nic Desilets <nic@ndesilets.com>
2026-01-18 14:11:07 -0800

    refactor: use UART instead of usb

--- esp-data-hub-2/main/main.c
+++ esp-data-hub-2/main/main.c
@@ -8,6 +8,8 @@
 #include <stdio.h>
 
 #include "cbor.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
 #include "esp_chip_info.h"
 #include "esp_flash.h"
 #include "esp_log.h"
@@ -17,18 +19,6 @@
 #include "math.h"
 #include "sdkconfig.h"
 #include "telemetry_types.h"
-#include "tinyusb.h"
-#include "tinyusb_cdc_acm.h"
-#include "tinyusb_default_config.h"
-#include "usb/cdc_acm_host.h"
-#include "usb/usb_host.h"
-
-#define EXAMPLE_USB_HOST_PRIORITY (20)
-#define EXAMPLE_USB_DEVICE_VID (0x303A)
-#define EXAMPLE_USB_DEVICE_PID (0x4001)       // 0x303A:0x4001 (TinyUSB CDC device)
-#define EXAMPLE_USB_DEVICE_DUAL_PID (0x4002)  // 0x303A:0x4002 (TinyUSB Dual CDC device)
-#define EXAMPLE_TX_STRING ("CDC test string!")
-#define EXAMPLE_TX_TIMEOUT_MS (1000)
 
 static const char* TAG = "data_hub";
 static SemaphoreHandle_t device_disconnected_sem;
@@ -92,6 +82,22 @@ void do_things_task(void* arg) {
   TickType_t last_wake = xTaskGetTickCount();
   uint32_t sequence = 0;
 
+  // uart config
+
+  uart_config_t uart_config = {
+      .baud_rate = 115200,
+      .data_bits = UART_DATA_8_BITS,
+      .parity = UART_PARITY_DISABLE,
+      .stop_bits = UART_STOP_BITS_1,
+      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+      .source_clk = UART_SCLK_DEFAULT,
+  };
+  int intr_alloc_flags = 0;
+
+  ESP_ERROR_CHECK(uart_driver_install(UART_NUM_0, 1024, 0, 0, NULL, intr_alloc_flags));
+  ESP_ERROR_CHECK(uart_param_config(UART_NUM_0, &uart_config));
+  ESP_ERROR_CHECK(uart_set_pin(UART_NUM_0, 43, 44, 15, 16));
+
   for (;;) {
     telemetry_packet_t packet = {
         .sequence = sequence,
@@ -121,104 +127,18 @@ void do_things_task(void* arg) {
     encode_telemetry_packet(&packet, cbor_buffer, sizeof(cbor_buffer), &encoded_size);
     cbor_buffer[encoded_size] = '\0';
 
-    for (size_t i = 0; i < encoded_size; ++i) {
-      printf("%02X", cbor_buffer[i]);
-      printf(" ");
-    }
-    printf("\n");
+    uart_write_bytes(UART_NUM_0, cbor_buffer, encoded_size);
+    ESP_LOGI(TAG, "sent %d bytes", encoded_size);
 
     sequence++;
     vTaskDelayUntil(&last_wake, period_ticks);
   }
 }
 
-static void handle_dev_event(const cdc_acm_host_dev_event_data_t* event, void* arg) {
-  switch (event->type) {
-    case CDC_ACM_HOST_ERROR:
-      ESP_LOGE(TAG, "CDC-ACM error has occurred, err_no = %i", event->data.error);
-      break;
-    case CDC_ACM_HOST_DEVICE_DISCONNECTED:
-      ESP_LOGI(TAG, "Device suddenly disconnected");
-      ESP_ERROR_CHECK(cdc_acm_host_close(event->data.cdc_hdl));
-      xSemaphoreGive(device_disconnected_sem);
-      break;
-    case CDC_ACM_HOST_SERIAL_STATE:
-      ESP_LOGI(TAG, "Serial state notif 0x%04X", event->data.serial_state.val);
-      break;
-    case CDC_ACM_HOST_NETWORK_CONNECTION:
-    default:
-      ESP_LOGW(TAG, "Unsupported CDC event: %i", event->type);
-      break;
-  }
-}
-
-static void usb_lib_task(void* arg) {
-  while (1) {
-    // Start handling system events
-    uint32_t event_flags;
-    usb_host_lib_handle_events(portMAX_DELAY, &event_flags);
-    if (event_flags & USB_HOST_LIB_EVENT_FLAGS_NO_CLIENTS) {
-      ESP_ERROR_CHECK(usb_host_device_free_all());
-    }
-    if (event_flags & USB_HOST_LIB_EVENT_FLAGS_ALL_FREE) {
-      ESP_LOGI(TAG, "USB: All devices freed");
-      // Continue handling USB events to allow device reconnection
-    }
-  }
-}
-
-static bool handle_rx(const uint8_t* data, size_t len, void* arg) {
-  ESP_LOGI(TAG, "Data received");
-  ESP_LOG_BUFFER_HEXDUMP(TAG, data, len, ESP_LOG_INFO);
-  return true;
-}
-
 void app_main(void) {
   printf("Hello world!\n");
 
-  device_disconnected_sem = xSemaphoreCreateBinary();
-  assert(device_disconnected_sem);
-
-  // Install USB Host driver. Should only be called once in entire application
-
-  ESP_LOGI(TAG, "Installing USB Host");
-  const usb_host_config_t host_config = {
-      .skip_phy_setup = false,
-      .intr_flags = ESP_INTR_FLAG_LEVEL1,
-  };
-  ESP_ERROR_CHECK(usb_host_install(&host_config));
-
-  // Create a task that will handle USB library events
-
-  BaseType_t task_created =
-      xTaskCreate(usb_lib_task, "usb_lib", 4096, xTaskGetCurrentTaskHandle(), EXAMPLE_USB_HOST_PRIORITY, NULL);
-  assert(task_created == pdTRUE);
-
-  ESP_LOGI(TAG, "Installing CDC-ACM driver");
-  ESP_ERROR_CHECK(cdc_acm_host_install(NULL));
-
-  // for device
-  const cdc_acm_host_device_config_t cdc_cfg = {
-      .connection_timeout_ms = 1000,
-      .out_buffer_size = 512,
-      .in_buffer_size = 512,
-      .user_arg = NULL,
-      .event_cb = handle_dev_event,
-      .data_cb = handle_rx,
-  };
+  // everything else
 
   xTaskCreate(do_things_task, "do_things", 2048, NULL, tskIDLE_PRIORITY + 1, NULL);
-
-  for (;;) {
-    cdc_acm_dev_hdl_t cdc_dev = NULL;
-
-    ESP_LOGI(TAG, "Opening CDC ACM device VID:PID=%04X:%04X", EXAMPLE_USB_DEVICE_VID, EXAMPLE_USB_DEVICE_PID);
-    esp_err_t err = cdc_acm_host_open(EXAMPLE_USB_DEVICE_VID, EXAMPLE_USB_DEVICE_PID, 0, &cdc_cfg, &cdc_dev);
-    if (err != ESP_OK) {
-      ESP_LOGI(TAG, "Failed to open device");
-      continue;
-    }
-    cdc_acm_host_desc_print(cdc_dev);
-    vTaskDelay(pdMS_TO_TICKS(100));
-  }
 }
